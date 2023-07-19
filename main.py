8cf60c47858bc394002e639f55d75249f8681e7c
lukas <113726590+BasedLukas@noreply.github.com>
2023-07-19 21:21:37 +0200

    readme

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 
 from zip_implementation import Zip
-
+from lz77 import LZ77Compressor
 
 
 
@@ -17,8 +17,14 @@ Your heart's been aching, but you're too shy to say it (say it)
 
 """
 
-z = Zip(return_type='binary')
+z = Zip(return_type='binary') #You can also return a string of 1 and 0 but then the length doesn't make sense anymore
+lz = LZ77Compressor()
+
+
+# compressed is a tuple of (compressed data, huffman codes, original length)
 compressed = z.compress(data)
+compressed_lz77 = lz.compress(data)
+
 
 del z # Show that nothing is being stored in the object
 z = Zip(return_type='binary')
@@ -26,13 +32,37 @@ z = Zip(return_type='binary')
 
 decompressed = z.decompress(compressed)
 
-print('Length of compressed data:',len(compressed[0])+ len(compressed[1]))  # This prints the length of the bytearray in bytes
+#convert huffman codes to easily readable format
+h_codes = {}
+for k,v in compressed[1].items():
+    if k[0] == 0:
+        h_codes[k[2]] = v
+    else:
+        h_codes[str(k[:2])] = v
+
+
+# convert lz77 codes to easily readable format
+to_print = []
+for item in compressed_lz77:
+    if item[0] == 0:
+        to_print.append(item[2])
+    else:
+        to_print.append(str(item[:2]))
+        to_print.append(item[2])
+to_print = ''.join(to_print)
+
+
+
+print('Length of compressed data:',len(compressed[0])+ len(compressed[1]))  
 print("Length of initial string:",len(data))  
 if (decompressed == data):
-    print("Compression worked with zero loss")  
+    print("Compression worked with zero loss") 
+else:
+    print("Compression failed") 
 
 print()
-print(f"Encoded string: \n{compressed[0]}")
+print(f"Encoded string: \n{''.join(format(byte, '08b') for byte in compressed[0])}")
 print()
-print(f"Huffman codes: \n{compressed[1]}")
-
+print(f"Huffman codes: \n{h_codes}")
+print()
+print(f"LZ77 Encoded string: \n{to_print}")
---
+++ readme.md
@@ -0,0 +1,10 @@
+## Gzip implementation
+
+This project is a simplified implementation of the gzip compression algorithm. The code here is used in my blogpost, which can be found [here](https://loreley.one/2023-07-gzip/).
+
+Run `main.py` in order to test the algorithm and view the output at various stages of compression.
+
+The `gzip_classification.py` file makes use of gzip as a text classifier. I explain how this is possible in the blog post. It is based on a paper entitled "Less is More: Parameter-Free Text Classification with Gzip".
+
+
+
